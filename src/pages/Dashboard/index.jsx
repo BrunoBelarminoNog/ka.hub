cef11e6787272239492f5e48831f657c407f570f
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-05-20 00:31:26 -0300

    fix: erro no deploy

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
 
 import api from "../../service/api";
 
